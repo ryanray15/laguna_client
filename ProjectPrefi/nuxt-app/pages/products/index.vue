6c3d5ca5cc040340066f7ef7fffb1258d5553074
Ryan <ryanlaguna28@gmail.com>
2024-05-12 00:11:20 +0800

    Final Output

---
+++ ProjectPrefi/nuxt-app/pages/products/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <div class="grid grid-cols-4 gap-5">
+        <div v-for="p in products">
+          <ProductCard :product="p" />
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+    const { data: products } = await useFetch('https://fakestoreapi.com/products')
+  
+    definePageMeta({
+      layout: "products",
+    })
+  
+    useHead({
+      title: 'Products',
+      meta: [
+        { name: 'description', content: 'Items'}
+      ]
+    })
+  </script>
